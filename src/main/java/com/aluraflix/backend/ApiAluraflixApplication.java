44102ff7e0afa3d8f49877a4b5fe218b71ee4142
BrunoHPLS <brunohpls.2001@gmail.com>
2022-04-12 20:05:25 -0300

    Initial Commit of Aluraflix

---
+++ src/main/java/com/aluraflix/backend/ApiAluraflixApplication.java
@@ -0,0 +1,13 @@
+package com.aluraflix.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAluraflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAluraflixApplication.class, args);
+	}
+
+}
